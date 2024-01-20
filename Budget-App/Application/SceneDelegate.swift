b69dcb9b763ba3c613a9b1bfc77422ee4464968c
Sahil Agashe <sahil@scrambleapps.gmail.com>
2024-01-20 13:42:18 +0530

    Implemented BudgetCategoriesTableViewController and BudgetModel

--- Budget-App/Application/SceneDelegate.swift
+++ Budget-App/Application/SceneDelegate.swift
@@ -6,10 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BudgetModel")
+        container.loadPersistentStores { (description: NSPersistentStoreDescription, error: Error?) in
+            if let error {
+                print("DEBUG SceneDelegate: Unable to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -18,7 +29,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        let nav = UINavigationController(rootViewController: BudgetCategoriesTableViewController(persistentContainer: persistentContainer))
+        window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
 
---
+++ Budget-App/Controller/BudgetCategoriesTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  BudgetCategoriesTableViewController.swift
+//  Budget-App
+//
+//  Created by SAHIL AMRUT AGASHE on 17/01/24.
+//
+
+import UIKit
+import CoreData
+
+class BudgetCategoriesTableViewController: UITableViewController {
+
+    // MARK: - Properties
+    private var persistentContainer: NSPersistentContainer
+    
+    // MARK: - Init
+    init(persistentContainer: NSPersistentContainer) {
+        self.persistentContainer = persistentContainer
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Thank you god for this learning opportunity!!!")
+    
+        setupUI()
+    }
+    
+    // MARK: - Selectors
+    @objc private func showAddBudgetCategory(_ sender: UIBarButtonItem) {
+        print("DEBUG BudgetCategoriesTableViewController: called function is \(#function)")
+    }
+    
+    // MARK: - Helpers
+    private func setupUI() {
+        let addBudgetCategoryButton = UIBarButtonItem(title: "Add Category", style: .plain, target: self, action: #selector(showAddBudgetCategory))
+        navigationItem.rightBarButtonItem = addBudgetCategoryButton
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Budget"
+    }
+
+
+}
+
